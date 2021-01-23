c687a16bc16cb3c1212055e5e852e7ca37a52d2f
cituk <programaciondyn3.mx@sandos.com>
2021-01-23 16:08:30 -0500

    Iniciar clase que implemente la interfaz repository con Eloquent

---
+++ app/Repositories/EloquentRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Repositories;
+
+
+class EloquentProjectRepository implements ProjectRepositoryInterface{
+
+    public function paginated(){
+
+    }
+    public function created(){
+
+    }
+    public function update(int $id){
+
+    }
+    public function delete(int $id){
+
+    }
+    public function find(int $id){
+
+    }
+
+}
