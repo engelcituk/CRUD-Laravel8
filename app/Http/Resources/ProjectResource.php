8b343f2c7c5a104084d06cef5b2d45e44fb5b590
cituk <programaciondyn3.mx@sandos.com>
2021-01-23 15:18:20 -0500

    Crear un resource

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "user" => $this->user->name,
+            "name" => $this->name,
+            "description" => $this->description,
+            "created_at" => $this->created_at,
+        ];
+    }
+}
